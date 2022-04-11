6c45081cc3a81913e1e80397f5b22d40105c10b0
Rory McStay <rory@rorymcstay.com>
2022-04-11 19:50:49 +0100

    work in progress, problems when we dont have mathcing sessions for fictional tickers

--- src/investment_analysis/tracking_error/strategy.py
+++ src/investment_analysis/tracking_error/strategy.py
@@ -18,56 +18,64 @@ from investment_analysis.zipline_ingester import VANGUARD_UNIVERSE
 
 logger = logging.getLogger(__name__)
 
-warnings.filterwarnings('ignore')
-
-
 def initialize(context: TradingAlgorithm):
     context.tick = 0
     context.window_size = 100
     context.rebal_interval = 30
     context.price_col = 'close'
-    context.assets = list(map(api.symbol, [
-        #'VUKE.L',
-        #'VMID.L',
+    context.traded_assets = list(map(api.symbol, [
+        'VUKE.L',
+        'VMID.L',
         'VUSA.L',
-        #'VGER.L',
-        #'VJPN.L',
+        'VGER.L',
     ]))
+    context.benchmarks = list(map(api.symbol, [
+        '^FTSE',
+        '^FTMC',
+        '^GSPC',
+        '^GDAXI',
+    ]))
+    assert len(context.benchmarks) == len(context.traded_assets)
 
 def handle_data(context: TradingAlgorithm, data: protocol.BarData):
     # Allow history to accumulate 100 days of prices before trading
-    # and rebalance every day thereafter.
+    # and rebalance every rebal_interval days thereafter.
     context.tick += 1
     if context.tick < context.window_size:
-    #    return
+        return
     if context.tick % context.rebal_interval != 0:
         # rebalance every 30 days
         return
     # Get rolling window of past prices and compute returns
-    prices = data.history(
-            assets=list(filter(data.can_trade, context.assets)),
+    benchmark_prices = data.history(
+            assets=list(filter(data.can_trade, context.benchmarks)),
             bar_count=context.window_size,
             fields=[context.price_col],
             frequency='1d')[context.price_col]
+    prices = data.history(
+            assets=list(filter(data.can_trade, context.traded_assets)),
+            bar_count=context.window_size,
+            fields=[context.price_col],
+            frequency='1d')[context.price_col]
+
+
     returns: pd.DataFrame = prices \
                 .pct_change() \
                 .unstack() \
                 .dropna()
-    returns.cumprod
-    # Perform Markowitz-style portfolio optimization
-    try:
-        weights, _, _ = computation.optimal_portfolio(returns.T)
-    except ValueError as ex:
-        logger.warning("failed to compute optimal portfolio %s", ex.args)
-        return
-    # Rebalance portfolio accordingly
-    weights = [[1] for _ in context.assets]
-    out = {}
-    weights = np.array([w[0] for w in weights])
-    normalized_weights = weights/sum(weights)
-    for stock, weight in zip(returns.columns, normalized_weights):
-        out[stock.symbol] = weight
-        api.order_target_percent(stock, weight)
+    benchmark_returns: pd.DataFrame = benchmark_prices \
+                .pct_change() \
+                .unstack() \
+                .dropna()
+    benchmark_value = benchmark_returns.add(1).cumsum().iloc[[-1]]
+    traded_value = returns.add(1).cumsum().iloc[[-1]]
+    for bench, asset in zip(context.traded_assets, context.benchmarks):
+        tracking_error = (benchmark_value[bench] - traded_value[asset]).iloc[[-1]]
+        out['tracking_error_{asset.symbol}'] = tracking_error
+    #for stock, benchmark, weight in zip(returns.columns, benchmark_returns.columns, normalized_weights):
+    #    diff = benchmark_returns[benchmark].period_value - returns[stock].period_value
+    #    out[stock.symbol] = weight
+    #    api.order_target_percent(stock, weight)
     api.record(**out)
 
 
@@ -88,8 +96,8 @@ if __name__ == '__main__':
             #analyze=analyze,
             benchmark_returns=benchmark_returns,
             capital_base=27000,
-            bundle='vanguard-etf-universe',
+            bundle='yahoo-finance-universe',
             data_frequency='daily'
     )
 
-    result.to_pickle('./backtest.pickle')
+    result.to_pickle('./tracking_error/backtest.pickle')
--- src/investment_analysis/zipline_ingester.py
+++ src/investment_analysis/zipline_ingester.py
@@ -3,6 +3,8 @@ from datetime import datetime
 import logging
 from typing import Dict
 
+import pytz
+
 import yfinance as yf
 import pandas as pd
 import numpy as np
@@ -37,90 +39,102 @@ VANGUARD_UNIVERSE = {
     'VUCP.L': 19,
     'VUTY.L': 20,
     'VEMT.L': 21,
+
+    '^FTSE': 22,
+    '^GSPC': 23,
+    '^FTMC': 24,
+    '^GDAXI': 25,
+
 }
 
-PERIOD = '5y'
+PERIOD = '1y'
 
 def df_cache_key(name, period, symbol):
     now = datetime.now()
     return f'{name}_{period}_{symbol}_{now.year}_{now.month}_{now.day}'
 
 
-def _yield_asset_from_yfinance(universe,
-        adjustment_writer: SQLiteAdjustmentWriter,
-        cache: Dict[str, pd.DataFrame]):
-    ticker: yf.Ticker
-
-    for symbol, ticker in universe.tickers.items():
+def yahoo_finance(environ: Dict[str, str],
+       asset_db_writer: AssetDBWriter,
+       minute_bar_writer: BcolzMinuteBarWriter,
+       daily_bar_writer: BcolzDailyBarWriter,
+       adjustment_writer: SQLiteAdjustmentWriter,
+       calendar: TradingCalendar,
+       start_session: pd.Timestamp,
+       end_session: pd.Timestamp,
+       cache: Dict[str, pd.DataFrame],
+       show_progress: bool,
+       output_dir):
+    """"""
+    universe = yf.Tickers(list(VANGUARD_UNIVERSE.keys()))
+    assets = []
+    info_cache_key = str(" ".join(tuple(k for k in universe.tickers.keys())))
+    if info_cache_key not in cache:
+        ticker: yf.Ticker
+        for symbol, ticker in universe.tickers.items():
+            info = ticker.get_info()
+            key = df_cache_key('history', PERIOD, symbol)
+            history = cache[key] if key in cache else ticker.history(PERIOD)
+            cache[key] = history
+            assets.append({
+                'sid': VANGUARD_UNIVERSE[symbol],
+                'symbol': symbol,
+                'asset_name': info['shortName'],
+                'exchange': info['exchange'],
+                'start_date': history.iloc[[0]].index[0],
+                'first_traded': history.iloc[[0]].index[0],
+                'end_date': history.iloc[[-1]].index[0]
+            })
+        equities = pd.DataFrame(data=assets)
+    else:
+        equities = cache[info_cache_key]
+    cache[info_cache_key] = equities
+    equities.index = equities.sid
+    print(equities)
+    asset_db_writer.write(equities=equities, chunk_size=100)
+    for asset in equities.itertuples():
+        print(asset)
+        symbol = asset.symbol
         cache_key = df_cache_key('history', PERIOD, symbol)
         data = ticker.history(PERIOD) if cache_key not in cache else cache[cache_key]
         sid = VANGUARD_UNIVERSE[symbol]
         data.columns = [c.lower().replace(' ', '_') for c in data.columns]
-        ohlc = data[['open', 'high', 'low', 'close', 'volume']]
-        stock_splits: pd.DataFrame = data[data.stock_splits != 0.0][['stock_splits']]
-        stock_splits['sid'] = sid
-        stock_splits['ratio'] = stock_splits.stock_splits
-        stock_splits['effective_date'] = stock_splits.index.astype(np.int64)
-        stock_splits = stock_splits[['ratio', 'sid', 'effective_date']]
-        stock_splits = stock_splits.reset_index(drop=True)
-        dividends = data[data['dividends'] > 0]['dividends']
-        dividends_ = pd.DataFrame()
-        dividends.index = dividends.index
-        dividends_['pay_date'] = dividends.index
-        dividends_['ex_date'] = dividends.index
-        dividends_['declared_date'] = dividends.index
-        dividends_['record_date'] = dividends.index
-        dividends_['amount'] = dividends
-        adjustment_writer.write(splits=stock_splits, dividends=dividends_)
-        yield (sid, ohlc)
+        ohlc: pd.DataFrame = data[['open', 'high', 'low', 'close', 'volume']]
+        sessions = calendar.sessions_in_range(data.iloc[[0]].index[0], data.iloc[[-1]].index[0])
+        ohlc_clean = ohlc.dropna()
+        ohlc_clean.index = ohlc_clean.index.tz_localize(pytz.utc)
+        ohlc_clean = ohlc_clean.reindex(sessions)
+        print(set(ohlc.index.values) - set(ohlc_clean.index.values))
+        daily_bar_writer.write([(sid,ohlc_clean)], invalid_data_behavior='warn')
+        stock_splits: pd.DataFrame = data[data.stock_splits != 0.0][['stock_splits']].dropna()
+        if not stock_splits.empty:
+            stock_splits['sid'] = sid
+            stock_splits['ratio'] = stock_splits.stock_splits
+            stock_splits['effective_date'] = stock_splits.index.view(np.int64)
+            stock_splits = stock_splits[['ratio', 'sid', 'effective_date']]
+            stock_splits = stock_splits.reset_index(drop=True)
+            stock_splits.index = stock_splits.sid
+            print(symbol, stock_splits)
+            try:
+                adjustment_writer.write(splits=stock_splits)
+            except ValueError as ex:
+                logger.error("Couldn't wrtie stock split data!")
+                if ex.args[0] != 'At least one valid asset id is required.':
+                    raise ex
 
-
-def vanguard_universe():
-
-    def _ingest_yfinance(environ: Dict[str, str],
-           asset_db_writer: AssetDBWriter,
-           minute_bar_writer: BcolzMinuteBarWriter,
-           daily_bar_writer: BcolzDailyBarWriter,
-           adjustment_writer: SQLiteAdjustmentWriter,
-           calendar: TradingCalendar,
-           start_session: pd.Timestamp,
-           end_session: pd.Timestamp,
-           cache: Dict[str, pd.DataFrame],
-           show_progress: bool,
-           output_dir):
-        """"""
-        universe = yf.Tickers(list(VANGUARD_UNIVERSE.keys()))
-        assets = []
-        info_cache_key = str(" ".join(tuple(k for k in universe.tickers.keys())))
-        if info_cache_key not in cache:
-            ticker: yf.Ticker
-            for symbol, ticker in universe.tickers.items():
-                info = ticker.get_info()
-                key = df_cache_key('history', PERIOD, symbol)
-                history = cache.get(key) or ticker.history(PERIOD)
-                cache[key] = history
-                assets.append({
-                    'sid': VANGUARD_UNIVERSE[symbol],
-                    'symbol': symbol,
-                    'asset_name': info['longName'],
-                    'exchange': info['exchange'],
-                    'start_date': history.iloc[[0]].index[0],
-                    'first_traded': history.iloc[[0]].index[0],
-                    'end_date': history.iloc[[-1]].index[0]
-                })
-            equities = pd.DataFrame(data=assets)
-        else:
-            equities = cache[info_cache_key]
-        cache[info_cache_key] = equities
-        equities.index = equities.sid
-        print(equities)
-        asset_db_writer.write(equities=equities)
-        daily_bar_writer.write(
-                _yield_asset_from_yfinance(
-                    universe=universe,
-                    adjustment_writer=adjustment_writer,
-                    cache=cache),
-                show_progress=True,
-                invalid_data_behavior='raise'
-                )
-    return _ingest_yfinance
+        dividends: pd.Series = data[data['dividends'] > 0]['dividends'].squeeze().dropna()
+        if not dividends.empty:
+            dividends_ = pd.DataFrame()
+            dividends_['pay_date'] = dividends.index
+            dividends_['ex_date'] = dividends.index
+            dividends_['declared_date'] = dividends.index
+            dividends_['record_date'] = dividends.index
+            dividends_['amount'] = dividends.values
+            dividends_['sid'] = sid
+            print(symbol, dividends_)
+            try:
+                adjustment_writer.write(dividends=dividends_)
+            except ValueError as ex:
+                logger.error("Couldn't wrtie dividend data!")
+                if ex.args[0] != 'At least one valid asset id is required.':
+                    raise ex
